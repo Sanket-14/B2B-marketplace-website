d4578b4ff5b91f8119a7a0d58fca332f58549b10
Sanket-14 <144524849+Sanket-14@users.noreply.github.com>
2024-05-18 22:26:37 +0530

    Update dbcon.php

--- dbcon.php
+++ dbcon.php
@@ -1,14 +1,1 @@
-<?php
-$servername = "localhost";
-$username = "thevonk2_Sanket";
-$password = "Sanket@123"; // Ensure it matches the MySQL root user's password
-$dbname = "thevonk2_voltbridge"; // Replace "your_database_name" with your actual database name
 
-// Establishing the connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-// Checking the connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-?>
