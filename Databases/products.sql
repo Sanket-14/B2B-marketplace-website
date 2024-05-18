2dfb277cb04f945899a44a8f73bbbddf45d1da8b
Sanket-14 <144524849+Sanket-14@users.noreply.github.com>
2024-05-18 22:24:55 +0530

    Delete Databases directory

--- Databases/products.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE products (
-    product_name VARCHAR(255) PRIMARY KEY,
-    comp_name VARCHAR(255),
-    product_image VARCHAR(255),
-    short_description VARCHAR(200),
-    product_description VARCHAR(5000)
-);
